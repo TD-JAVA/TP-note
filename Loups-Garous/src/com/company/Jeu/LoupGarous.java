1681e8be507d5e37a9ae88f9fa4d8c066e5521aa
Gab55 <gabrielgiroud@gmail.com>
2016-11-29 11:16:01 +0100

    Etape de base
    Création des classes de base

---
+++ Loups-Garous/src/com/company/Jeu/LoupGarous.java
@@ -0,0 +1,7 @@
+package com.company.Jeu;
+
+/**
+ * Created by Gaby on 29/11/2016.
+ */
+public class LoupGarous {
+}
---
+++ Loups-Garous/src/com/company/Jeu/Main.java
@@ -0,0 +1,11 @@
+package com.company.Jeu;
+
+/**
+ * Created by Gaby on 29/11/2016.
+ */
+public class Main {
+
+        public static void main(String[] args) {
+            // write your code here
+        }
+}
