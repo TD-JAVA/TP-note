ae0a654fd9d303dae4a16d889614f0ec30ed880c
Gab55 <gabrielgiroud@gmail.com>
2016-12-04 21:11:11 +0100

    Changements mineurs
    Code en plus

--- Loups-Garous/src/com/company/Jeu/LoupGarous.java
+++ Loups-Garous/src/com/company/Jeu/LoupGarous.java
@@ -4,4 +4,27 @@ package com.company.Jeu;
  * Created by Gaby on 29/11/2016.
  */
 public class LoupGarous {
+    private String nom;
+    private int num;
+
+    public LoupGarous(String nom, int num) {
+        this.nom = nom;
+        this.num = num;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
 }
--- Loups-Garous/src/com/company/Jeu/Villageois.java
+++ Loups-Garous/src/com/company/Jeu/Villageois.java
@@ -5,4 +5,27 @@ package com.company.Jeu;
  */
 public class Villageois {
     private String nom;
+    private  int numVillageois;
+
+
+    public Villageois(String nom, int numVillageois) {
+        this.nom = nom;
+        this.numVillageois = numVillageois;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getNumVillageois() {
+        return numVillageois;
+    }
+
+    public void setNumVillageois(int numVillageois) {
+        this.numVillageois = numVillageois;
+    }
 }
